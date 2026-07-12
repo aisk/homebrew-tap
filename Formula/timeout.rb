54e40b3b143d8d42334232067ca3a052c1f30273
An Long <aisk1988@gmail.com>
2026-07-12 15:57:52 +0900

    Update timeout to 0.1.2

--- Formula/timeout.rb
+++ Formula/timeout.rb
@@ -1,8 +1,8 @@
 class Timeout < Formula
   desc "Simple timeout command implementation"
   homepage "https://github.com/aisk/timeout"
-  url "https://github.com/aisk/timeout/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "94edef9eae4c21a36be42b2769b5b2b6b2fc57f73590096ef12eaff33de6ee71"
+  url "https://github.com/aisk/timeout/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "8859dbd3860f9046cc2eb9ca213eceba03dd2a0889623a4d4b919e8b54d70362"
   head "https://github.com/aisk/timeout.git", branch: "master"
   license "GPL-3.0"
 
